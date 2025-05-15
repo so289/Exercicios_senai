1445e0b8accb96624e474708dea0dacdb1635dd3
so289 <sofiaa.costa120408@gmail.com>
2025-05-15 15:35:38 -0300

    Add files via upload

---
+++ dia_da semana.py
@@ -0,0 +1,18 @@
+numero=input('digite um numero:')
+if numero == '1':
+    print('domingo')
+elif numero=='2':
+    print('segunda-feira')
+elif numero =='3':
+    print('terça-feira')
+elif numero== '4':
+    print('quarta-feira')
+elif numero=='5':
+    print('quinta-feira')
+elif numero=='6':
+    print('sexta-feira')
+elif numero== '7':
+    print('sábado')
+else:
+    print('número errado')
+
